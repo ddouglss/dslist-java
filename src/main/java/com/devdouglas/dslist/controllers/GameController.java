9add373e3bc5faa57853005f5f75ba815ad6a55d
ddouglss <ddouglss1999@gmail.com>
2025-05-21 00:10:09 -0300

    Get game by id

--- src/main/java/com/devdouglas/dslist/controllers/GameController.java
+++ src/main/java/com/devdouglas/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.devdouglas.dslist.controllers;
 
+import com.devdouglas.dslist.dto.GameDTO;
 import com.devdouglas.dslist.dto.GameMinDTO;
 import com.devdouglas.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,12 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO dto = gameService.findById(id);
+        return dto;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll(){
--- src/main/java/com/devdouglas/dslist/services/GameService.java
+++ src/main/java/com/devdouglas/dslist/services/GameService.java
@@ -1,10 +1,13 @@
 package com.devdouglas.dslist.services;
 
+import com.devdouglas.dslist.dto.GameDTO;
 import com.devdouglas.dslist.dto.GameMinDTO;
 import com.devdouglas.dslist.entities.Game;
+import com.devdouglas.dslist.exceptions.ResourceNotFoundException;
 import com.devdouglas.dslist.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +17,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game game = gameRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Game with ID " + id + " not found"));
+        return new GameDTO(game);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
       List<Game> result = gameRepository.findAll();
       List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
